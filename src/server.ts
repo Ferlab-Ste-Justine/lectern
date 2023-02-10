df992f90d6f74f06987469cf6c0ab18a826a111c
Eric Vallee <eric_vallee@webificservices.com>
2023-02-10 01:23:57 -0500

    feat: INFRA-228 Added support for tls. Made exit code return non-zero on error. Made server start only if mongodb connection doesn't produce an error

---
+++ src/server.ts
@@ -0,0 +1,70 @@
+import App from './app';
+import mongoose from 'mongoose';
+import { constructMongoUri } from './utils/mongo';
+import logger from './config/logger';
+import { Server } from 'http';
+import { getAppConfig } from './config/appConfig';
+import { readFileSync } from 'fs';
+
+let server: Server;
+
+(async () => {
+  const appConfig = await getAppConfig();
+
+  /** Mongoose setup */
+  mongoose.connection.on('connecting', () => {
+    logger.info('Connecting to MongoDB...');
+  });
+  mongoose.connection.on('connected', () => {
+    logger.info('...Connection Established to MongoDB');
+  });
+  mongoose.connection.on('reconnected', () => {
+    logger.info('Connection Reestablished');
+  });
+  mongoose.connection.on('disconnected', () => {
+    logger.warn('Connection Disconnected');
+  });
+  mongoose.connection.on('close', () => {
+    logger.warn('Connection Closed');
+  });
+  mongoose.connection.on('error', error => {
+    logger.error('MongoDB Connection Error:' + error);
+  });
+  mongoose.connection.on('reconnectFailed', () => {
+    logger.error('Ran out of reconnect attempts, abandoning...');
+  });
+
+  Promise.resolve(true)
+    .then(() => {
+      const caFile = appConfig.mongoTlsCAFile();
+      return caFile == '' ? Buffer.from('') : readFileSync(caFile);
+    })
+    .then((caCert: Buffer) => {
+      return mongoose.connect(constructMongoUri(appConfig), {
+        autoReconnect: true,
+        socketTimeoutMS: 10000,
+        connectTimeoutMS: 30000,
+        keepAlive: true,
+        reconnectTries: 10,
+        reconnectInterval: 3000,
+        useNewUrlParser: true,
+        ssl: appConfig.mongoTls(),
+        sslValidate: appConfig.mongoTls(),
+        sslCA: [caCert],
+      });
+    })
+    .then(() => {
+      /**
+       * Start Express server.
+       */
+      const app = App(appConfig);
+      server = app.listen(app.get('port'), () => {
+        logger.info(`App is running at http://localhost:${app.get('port')} in ${app.get('env')} mode`);
+        logger.info('Press CTRL-C to stop');
+      });
+    })
+    .catch((err: Error) => {
+      logger.error('MongoDB connection error. Please make sure MongoDB is running. ' + err);
+      process.exit(1);
+    });
+})();
